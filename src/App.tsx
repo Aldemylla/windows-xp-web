24e22d75952a4cb71b18fd4bff5ca7607c077101
Aldemylla <aldemylla@gmail.com>
2022-07-08 17:29:35 -0300

    Start configs

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className='App'>
+      <img src='/assets/acessories-calculator.svg' alt='' />
+      <p>Home</p>
+    </div>
+  );
+}
+
+export default App;
+
